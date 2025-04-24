22b27711c68899206903709834dab987e1b55bf3
pravinnagar491 <47731905+pravinnagar491@users.noreply.github.com>
2025-04-24 15:48:05 +0530

    Update main.js
    added comment and remove console output

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 
 function fetchJSONData() {
+    //fetch json file
     fetch('./code-test.json')
         .then(response => {
             if (!response.ok) {
@@ -8,21 +9,32 @@ function fetchJSONData() {
             return response.json();  
         })
         .then(data => {
-            renderData(data);
+
+            if (data) {
+                if (data !== null && data !== undefined) {
+                     // Access Data
+                    renderData(data);
+                } else {
+                  // Handle null or undefined data
+                  console.log("response 404")
+                }
+              } else {
+                // Handle null or undefined data
+                console.log("response 404")
+              }
+            
         })  
         .catch(error => console.error('Failed to fetch data:', error)); 
 }
 
 window.onload = fetchJSONData();  
 
-
+//created function for genrate dynamic HTML and render data.
 function renderData(data){
-    console.log(data.articles);
     var htmlTag = "";
     var getoutput = data.articles.forEach((item, index)=>{
         htmlTag += '<div class="card-div"><label class="category">'+item.primarySectionRouteName+'</label><h1 class="title">'+item.headline+'</h1><div class="midsection"><div class="img-section"><img src="'+item.thumbnail.src+'" alt="'+item.thumbnail.title+'"></div><div class="para-section"><p class="standfirst">'+item.standfirst+'</p><h1 class="title">'+item.headline+'</h1></div></div></div>';
     })
 
-    document.getElementById("myList").insertAdjacentHTML('afterend', htmlTag);;
-    console.log(htmlTag);
+    document.getElementById("myList").insertAdjacentHTML('afterend', htmlTag);
 }
